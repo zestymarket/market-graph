b8971d357962691b3cb60be086c222e12891c790
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-05-31 22:52:36 +0800

    create zestyNFT mappings

--- src/ZestyNFT.ts
+++ src/ZestyNFT.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, store } from "@graphprotocol/graph-ts"
 import {
   ZestyNFT,
   Approval,
@@ -11,76 +11,49 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/ZestyNFT/ZestyNFT"
-import { ExampleEntity } from "../generated/schema"
+import { ExampleEntity, TokenData } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.baseURI(...)
-  // - contract.getApproved(...)
-  // - contract.getTokenData(...)
-  // - contract.getZestyTokenAddress(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleBurn(event: Burn): void {}
+export function handleBurn(event: Burn): void {
+  let id = event.params.id.toString();
+  store.remove("TokenData", id);
+}
 
-export function handleLockZestyToken(event: LockZestyToken): void {}
+export function handleLockZestyToken(event: LockZestyToken): void {
+  let entity = new TokenData(event.params.id.toString()); 
 
-export function handleMint(event: Mint): void {}
+  entity.zestyTokenValue = event.params.zestyTokenValue;
+  entity.save();
+}
 
-export function handleModifyToken(event: ModifyToken): void {}
+export function handleMint(event: Mint): void {
+  let entity = new TokenData(event.params.id.toString());
+
+  entity.id = event.params.id.toString();
+  entity.publisher = event.params.creator;
+  entity.owner = event.params.creator;
+  entity.timeCreated = event.params.timeCreated;
+  entity.uri = event.params.uri;
+  entity.save();
+}
+
+export function handleModifyToken(event: ModifyToken): void {
+  let entity = new TokenData(event.params.id.toString());
+
+  entity.uri = event.params.uri;
+  entity.save();
+}
 
 export function handleNewZestyTokenAddress(event: NewZestyTokenAddress): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let entity = new TokenData(event.params.id.toString());
+
+  entity.owner = event.params.to;
+  entity.save();
+}
