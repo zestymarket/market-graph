7b152b3c5aa6248c34d7f2989850418e7712c72a
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-06-02 00:31:32 +0800

    zestymarket graph done

--- src/ZestyMarketUSDC.ts
+++ src/ZestyMarketUSDC.ts
@@ -49,30 +49,80 @@ export function handleSellerNFTWithdraw(event: SellerNFTWithdraw): void {
   store.remove("SellerNFTSetting", id);
 }
 
-export function handleWithdrawZestyNFT(event: WithdrawZestyNFT): void {}
+export function handleBuyerCampaignCreate(event: BuyerCampaignCreate): void {
+  let entity = new BuyerCampaign(event.params.buyerCampaignId.toString());
+  entity.buyer = event.params.buyer;
+  entity.uri = event.params.uri;
+  entity.save();
+}
 
-export function handleBuyerCampaignCancel(event: BuyerCampaignCancel): void {}
+export function handleBuyerCampaignCancel(event: BuyerCampaignCancel): void {
+  let id = event.params.buyerCampaignId.toString();
+  store.remove("BuyerCampaign", id);
+}
 
-export function handleBuyerCampaignCreate(event: BuyerCampaignCreate): void {}
+export function handleSellerAuctionCreate(event: SellerAuctionCreate): void {
+  let entity = new SellerAuction(event.params.sellerAuctionId.toString());
 
-export function handleContractCreate(event: ContractCreate): void {}
+  entity.currency = "usdc";
+  entity.sellerNFTSetting = event.params.tokenId.toString();
+  entity.seller = event.params.seller;
+  entity.auctionTimeStart = event.params.auctionTimeStart;
+  entity.auctionTimeEnd = event.params.auctionTimeEnd;
+  entity.contractTimeStart = event.params.contractTimeStart;
+  entity.contractTimeEnd = event.params.contractTimeEnd;
+  entity.priceStart = event.params.priceStart;
+  entity.priceEnd = new BigInt(0);
+  entity.buyerCampaignApproved = event.params.buyerCampaignApproved == 2 ? true : false;
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  entity.save();
+}
 
-
-export function handleSellerAuctionBuyerCampaignApprove(
-  event: SellerAuctionBuyerCampaignApprove
-): void {}
+export function handleSellerAuctionCancel(event: SellerAuctionCancel): void {
+  let id = event.params.sellerAuctionId.toString();
+  store.remove("SellerAuction", id);
+}
 
 export function handleSellerAuctionBuyerCampaignNew(
   event: SellerAuctionBuyerCampaignNew
-): void {}
+): void {
+  let entity = new SellerAuction(event.params.sellerAuctionId.toString());
+
+  entity.buyerCampaign = event.params.buyerCampaignId.toString();
+  entity.priceEnd = event.params.priceEnd;
+  entity.save();
+}
+
+export function handleSellerAuctionBuyerCampaignApprove(
+  event: SellerAuctionBuyerCampaignApprove
+): void {
+  let entity = new SellerAuction(event.params.sellerAuctionId.toString());
+  entity.buyerCampaignApproved = true;
+  entity.save();
+}
+
 
 export function handleSellerAuctionBuyerCampaignReject(
   event: SellerAuctionBuyerCampaignReject
-): void {}
+): void {
+  let entity = new SellerAuction(event.params.sellerAuctionId.toString());
+  entity.buyerCampaignApproved = false;
+  entity.save();
+}
 
-export function handleSellerAuctionCancel(event: SellerAuctionCancel): void {}
-
-export function handleSellerAuctionCreate(event: SellerAuctionCreate): void {}
+export function handleContractCreate(event: ContractCreate): void {
+  let entity = new Contract(event.params.contractId.toString());
+  entity.sellerAuction = event.params.sellerAuctionId.toString();
+  entity.buyerCampaign = event.params.buyerCampaignId.toString();
+  entity.contractTimeStart = event.params.contractTimeStart;
+  entity.contractTimeEnd = event.params.contractTimeEnd;
+  entity.contractValue = event.params.contractValue;
+  entity.withdrawn = false;
+  entity.save();
+}
+export function handleContractWithdraw(event: ContractWithdraw): void {
+  let entity = new Contract(event.params.contractId.toString());
+  entity.withdrawn = true;
+  entity.save();
+}
 
